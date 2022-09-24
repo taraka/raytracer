b137b1e507abf80acd139078fee22b59cf7ea898
Tom Rawcliffe <git@tomrawcliffe.co.uk>
2022-09-24 22:28:57 +0100

    Adding basic striped pattern

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod intersection;
 mod light;
 mod material;
 mod matrix;
+mod pattern;
 mod plane;
 mod ray;
 mod shape;
---
+++ src/pattern/stripe.rs
@@ -0,0 +1,63 @@
+use crate::color::Color;
+use crate::tuple::*;
+
+pub struct StripePattern {
+    pub a: Color,
+    pub b: Color,
+}
+
+impl StripePattern {
+    fn new(a: Color, b: Color) -> Self {
+        Self {
+            a,
+            b,
+        }
+    }
+
+    fn color_at(&self, p: &Tuple) -> Color {
+        if p.x.floor().abs() as isize % 2 == 0 {
+            self.a
+        } else {
+            self.b
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::pattern::stripe::*;
+
+    #[test]
+    fn create_stripe_pattern() {
+        let p = StripePattern::new(Color::black(), Color::white());
+        assert_eq!(p.a, Color::black());
+        assert_eq!(p.b, Color::white());
+    }
+
+    #[test]
+    fn stripe_pattern_constant_in_y() {
+        let p = StripePattern::new(Color::white(), Color::black());
+        assert_eq!(p.color_at(&point(0.0, 0.0, 0.0)), Color::white());
+        assert_eq!(p.color_at(&point(0.0, 1.0, 0.0)), Color::white());
+        assert_eq!(p.color_at(&point(0.0, 2.0, 0.0)), Color::white());   
+    }
+
+    #[test]
+    fn stripe_pattern_constant_in_z() {
+        let p = StripePattern::new(Color::white(), Color::black());
+        assert_eq!(p.color_at(&point(0.0, 0.0, 0.0)), Color::white());
+        assert_eq!(p.color_at(&point(0.0, 0.0, 1.0)), Color::white());
+        assert_eq!(p.color_at(&point(0.0, 0.0, 2.0)), Color::white());   
+    }
+
+    #[test]
+    fn stripe_pattern_altinates_in_x() {
+        let p = StripePattern::new(Color::white(), Color::black());
+        assert_eq!(p.color_at(&point(0.0, 0.0, 0.0)), Color::white());
+        assert_eq!(p.color_at(&point(0.9, 0.0, 0.0)), Color::white());
+        assert_eq!(p.color_at(&point(1.0, 0.0, 0.0)), Color::black());   
+        assert_eq!(p.color_at(&point(-0.1, 0.0, 0.0)), Color::black());   
+        assert_eq!(p.color_at(&point(-1.0, 0.0, 0.0)), Color::black()); 
+        assert_eq!(p.color_at(&point(-1.1, 0.0, 0.0)), Color::white()); 
+    }
+}
